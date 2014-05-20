0627c9d27539a0de88202b09574eb553f58dd8ac
Stefan Haustein <stefan.haustein@gmail.com>
2014-05-20 19:18:01 -0400

    started  basic support for globals, binary operators and calls

--- src/jstypecheck.js
+++ src/jstypecheck.js
@@ -44,17 +44,84 @@ JsTypeCheck.prototype.check = function(ast) {
 	}
 };
 
+JsTypeCheck.prototype.assertAssignableFrom = function(node, targetType, sourceType) {
+    if (targetType.type != sourceType.type) {
+        this.error(node, "cannot assign " + JSON.stringify(sourceType) + " to " + JSON.stringify(targetType));
+    }
+};
+
+JsTypeCheck.prototype.checkBinaryExpression = function(expr) {
+    var left = this.checkExpression(expr.left);
+    var right = this.checkExpression(expr.right);
+    var jsType = JsTypeCheck.TYPE_ANY;
+    
+    switch(expr.operator) {
+        case "+": 
+            if (left.type == "number" && right.type == "number") {
+               jsType = JsTypeCheck.TYPE_NUMBER; 
+            } else {
+                jsType = JsTypeCheck.TYPE_STRING;
+            }
+            break;
+        
+        case "<":
+        case "<=":
+        case ">":
+        case ">=":
+        case "==":
+        case "===":
+        case "!==":
+        case "!+":
+            jsType = JsTypeCheck.TYPE_BOOLEAN;
+            break;
+            
+        case "-":
+        case "*":
+        case "&":
+        case "|":
+        case "^":
+            jsType = JsTypeCheck.TYPE_NUMBER;
+            break;
+            
+        default:
+            console.log("Unsupported binary operator: '" + expr.operator + "'");
+    }
+    
+    expr.jsType = jsType;
+    return jsType;
+};
+
 JsTypeCheck.prototype.checkBlockStatement = function(fn) {
 	this.debug('BlockStatement', fn);
 	var body = fn.body;
 	for (var i = 0; i < body.length; i++) {
-		var statement = body[i];
-		if (statement.type == "VariableDeclaration") {
-			this.checkVariableDeclaration(statement);
-		} else {
-			console.log("skipping", statement);
-		}
+	    this.checkStatement(body[i]);
 	}
+};
+
+JsTypeCheck.prototype.checkCallExpression = function(call) {
+    var calleeType = this.checkExpression(call.callee);
+    if (calleeType.type != "function") {
+        error(call, "function expected");
+    }
+    var args = call.arguments || [];
+    var params = calleeType.parameter || [];
+    if (args.length > params.length) {
+        error(call, '' + params.length + ' arguments expected; got ' + arguments.length);
+    }
+    for (var i = 0; i < params.length; i++) {
+        if (i >= args.length) {
+            if (!(params[i].optional)) {
+                error(call, 'non-optional parameter ' + i + ' is missing.');
+            }
+        } else {
+            var argType = this.checkExpression(args[i]);
+            this.assertAssignableFrom(call, params[i], argType);
+        }
+    }
+    
+    call.jsType = calleeType.returns;
+    return call.jsType;
 };
 
 JsTypeCheck.prototype.checkFunctionDeclaration = function(fn) {
@@ -64,13 +131,26 @@ JsTypeCheck.prototype.checkFunctionDeclaration = function(fn) {
 	if (fn.body.type == "BlockStatement") {
 		this.checkBlockStatement(fn.body);
 	} else {
-		console.log("skipping " + node);
+		console.log("skipping " + fn);
 	}
 };
 
 JsTypeCheck.prototype.checkLiteral = function(literal) {
 	// We can do better for object and array literals...
 	return this.getValueType(literal.value);
+};
+
+JsTypeCheck.prototype.checkStatement = function(statement) {
+	switch(statement.type) {
+    case "ExpressionStatement": 
+        this.checkExpression(statement.expression);
+        break;
+	case "VariableDeclaration":
+		this.checkVariableDeclaration(statement);
+        break;
+    default:
+		console.log("skipping", statement);
+	}
 };
 
 JsTypeCheck.prototype.checkVariableDeclaration = function(varStatement) {
@@ -91,9 +171,18 @@ JsTypeCheck.prototype.checkVariableDeclarator = function(varDecl) {
 JsTypeCheck.prototype.checkExpression = function(expr) {
 	var jsType;
 	switch(expr.type) {
+	case "BinaryExpression":
+	    jsType = this.checkBinaryExpression(expr);
+	    break;
+	case "CallExpression":
+	    jsType = this.checkCallExpression(expr);
+	    break;
 	case "Literal":
 		jsType = this.checkLiteral(expr);
 		break;
+	case "Identifier":
+	    jsType = this.checkIdentifier(expr);
+	    break;
 	default:
 		console.log("skipping expr: ", expr);
 		jsType = JsTypeCheck.TYPE_ANY;
@@ -101,6 +190,18 @@ JsTypeCheck.prototype.checkExpression = function(expr) {
 	expr.jsType = jsType;
 	return jsType;
 };
+
+JsTypeCheck.prototype.checkIdentifier = function(expr) {
+    var name = expr.name;
+    var jsType = this.locals[name] || this.declaredGlobals[name] ||
+        this.globals[name];
+    if (jsType == null) {
+        this.error(expr, "Undefined identifier '" + name + "'");
+    }
+    expr.jsType = jsType; 
+    return jsType;
+};
+
 
 JsTypeCheck.prototype.debug = function(fn, node) {
 	console.log(fn, node);
@@ -121,6 +222,7 @@ JsTypeCheck.prototype.getValueType = function(value) {
 	}
 };
 
+// TODO(haustein): Match esprima error format
 JsTypeCheck.prototype.error = function(node, msg) {
 	throw {
 		message: msg,
